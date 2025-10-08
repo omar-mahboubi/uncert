00e65f9dc9cf6cbc9ded66ef3f6d7fafc8351521
Omar Mahboubi <omar_mahboubi@tutanota.com>
2025-10-08 17:43:54 +0100

    uncert:cpp: Added a product function

--- uncert.cpp
+++ uncert.cpp
@@ -95,6 +95,34 @@ void sum (char sign) {
     cout << "The value of x1 + x2, adjusted for uncertainty, is: " << x << " + " << dx << endl;
 }
 
+void log_product() {
+    int n1, n2, power1, power2;
+    float sys1, sys2, a, b, c;
+    float array1[200], array2[200];
+    float x, dx;
+    cout << "Please input a, b, c, n, k in order\n" ;
+    cin >> a; cin >> b; cin >> c; cin >> power1; cin >> power2;
+    cout << "Input the number of measurements for x1.\n"; cin >> n1;
+    cout << "Please input the systematic uncertainty for x1 (in the same unit as the measurements)\n"; cin >> sys1;
+    cout << "Please input the measurements of x1, one-by-one:\n";
+    for (int i=0 ; i<n1 ; i++) {
+        cin >> array1[i];
+    }
+    print_line();
+    cout << "Input the number of measurements for x2.\n"; cin >> n2;
+    cout << "Please input the systematic uncertainty for x2 (in the same unit as the measurements)\n"; cin >> sys2;
+    cout << "Please input the measurements of x2, one-by-one:\n";
+    for (int i=0;i<n2;i++) {
+        cin >> array2[i];
+    }
+    print_line();
+    Result x1 = calculate (n1, sys1, array1);
+    Result x2 = calculate (n2, sys2, array2);
+    x = (a*pow(x1.avg,2)) * (b*pow(x2.avg,2)) + c ;
+    dx = x * ((x1.delta * power1 / x1.avg) + (x2.delta * power2 / x2.avg)) ;
+    cout << "The value of z, adjusted for uncertainty, is: " << x << " + " << dx << endl;
+}
+
 int main() {
     int choice;
     cout << "Welcome to Uncertainty Calculator!\n" ;
@@ -102,11 +130,13 @@ int main() {
     cout << "Please choose the calculation type (type the number of the type in question)\n" ;
     cout << "(1) Single variable x\n" ;
     cout << "(2) Sum of two variables x1 and x2\n" ;
+    cout << "(3) Product: z = ax^n * bx^k + c (Logarithmic method)\n" ;
     cin >> choice;
     print_line();
     switch (choice) {
         case (1): single(); break;
         case (2): sum('+'); break;
+        case (3): log_product(); break;
         default:
             cout << "Please choose a valid value!" << endl; return 1;
     }
